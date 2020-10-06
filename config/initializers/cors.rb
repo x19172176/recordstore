9acb8e614ee463f88b4c002c8ccdc4b9d45963ef
Eddie Stack <x19172176@student.ncirl.ie>
2020-10-06 03:01:22 +0100

    deploy 2

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:8081'
+    origins 'https://safe-ocean-96942.herokuapp.com/'
 
     resource '*',
       headers: :any,
